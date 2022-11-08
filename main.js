151ca99865a5be2d26766ddd69e9d04b16657b98
jaimezzapata <zapataval2304@gmail.com>
2022-11-08 07:33:58 -0500

    Plantilla base

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("Hola Mundo");
+let nombre = "Jaime";
+let apellido = "Zapata";
+
+console.log(nombre + apellido);
+
+if (true) {
+  console.log(nombre + apellido);
+
+  switch (true) {
+    case 1:
+      console.log("Primer Case");
+      break;
+  }
+}
